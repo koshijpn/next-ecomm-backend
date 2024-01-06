20d7030e23abd0dfa95b9d8323d404c6c1f7c947
KoshiSu <pcf7b44972xxo@gmail.com>
2024-01-07 00:31:13 +0700

    fly deployed ver.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from "./app.js"
-const port = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`App started; listening on port ${port}`)
-})
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
